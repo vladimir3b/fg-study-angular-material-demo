a99e7fadb632abb61ec45d2e4329b302b6d6d985
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2019-03-01 08:08:57 +0200

    basic fg-angular structure

---
+++ src/app/project-structure/root/root.module.ts
@@ -0,0 +1,32 @@
+/***
+ *    ██████╗  ██████╗  ██████╗ ████████╗
+ *    ██╔══██╗██╔═══██╗██╔═══██╗╚══██╔══╝
+ *    ██████╔╝██║   ██║██║   ██║   ██║
+ *    ██╔══██╗██║   ██║██║   ██║   ██║
+ *    ██║  ██║╚██████╔╝╚██████╔╝   ██║
+ *    ╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝
+ *
+ *    ███╗   ███╗ ██████╗ ██████╗ ██╗   ██╗██╗     ███████╗
+ *    ████╗ ████║██╔═══██╗██╔══██╗██║   ██║██║     ██╔════╝
+ *    ██╔████╔██║██║   ██║██║  ██║██║   ██║██║     █████╗
+ *    ██║╚██╔╝██║██║   ██║██║  ██║██║   ██║██║     ██╔══╝
+ *    ██║ ╚═╝ ██║╚██████╔╝██████╔╝╚██████╔╝███████╗███████╗
+ *    ╚═╝     ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+ *
+ */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RootComponent } from './components/root/root.component';
+
+@NgModule({
+  declarations: [
+    RootComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [ RootComponent ]
+})
+export class RootModule { }
