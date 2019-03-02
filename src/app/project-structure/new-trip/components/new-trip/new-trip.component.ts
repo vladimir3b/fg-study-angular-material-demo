27501a8202cc3ab23c82fa8ed13624cba3d29d49
VladimirIoan <vladimir_3b@hotmail.com>
2019-03-02 13:46:36 +0200

    basic forms ready

--- src/app/project-structure/new-trip/components/new-trip/new-trip.component.ts
+++ src/app/project-structure/new-trip/components/new-trip/new-trip.component.ts
@@ -28,7 +28,15 @@
  *    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝
  *
  */
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'fg-new-trip',
@@ -42,6 +50,11 @@ export class NewTripComponent implements OnInit {
  *    ├─┘├┬┘│ │├─┘├┤ ├┬┘ │ │├┤ └─┐
  *    ┴  ┴└─└─┘┴  └─┘┴└─ ┴ ┴└─┘└─┘
  */
+  public personalDataForm: FormGroup;
+  public contactDataForm: FormGroup;
+  public locationDataForm: FormGroup;
+  public travelingDatesForm: FormGroup;
+
 
 /***
  *    ┌─┐┌─┐┌┐┌┌─┐┌┬┐┬─┐┬ ┬┌─┐┌┬┐┌─┐┬─┐
@@ -62,7 +75,36 @@ export class NewTripComponent implements OnInit {
  *    ┴ ┴└─┘└─┘┴ ┴└─┘
  */
   public ngOnInit(): void {
-  }
+    // Creating model for Personal Data Form
+    this.personalDataForm = new FormGroup({
+      firstName: new FormControl(null, [ Validators.required ]),
+      lastName: new FormControl(null, [ Validators.required ]),
+      birthDate: new FormControl(null),
+      gender: new FormControl(null)
+    });
+
+    // Creating model for Contact Data Form
+    this.contactDataForm = new FormGroup({
+      phoneNumber: new FormControl(null, [ Validators.required ]),
+      email: new FormControl(null, [ Validators.required ]),
+      address: new FormControl(null, [ Validators.required ])
+    });
+
+    // Creating model for Location Data Form
+    this.locationDataForm = new FormGroup({
+      country: new FormControl(null),
+      city: new FormControl(null),
+      notes: new FormControl(null)
+    });
+
+    // Creating model for Traveling Dates Form
+    this.travelingDatesForm = new FormGroup({
+      departureDate: new FormControl(null),
+      departureHour: new FormControl(null),
+      arrivalDate: new FormControl(null),
+      arrivalHour: new FormControl(null)
+    });
+}
 
  /***
  *    ┌┬┐┌─┐┌┬┐┬ ┬┌─┐┌┬┐┌─┐
